33099bb99b9438a0ab526fc6feaaefa258ddc17a
mupotsal <46546491+mupotsal@users.noreply.github.com>
2020-04-21 00:07:00 -0400

    done

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+const url = "https://cfw-takehome.developers.workers.dev/api/variants";
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+	let thing =	{ 
+		method: 'GET'
+  }
+  var urls = []
+	return fetch(url, thing)
+		.then(response => {
+			if (response.status === 200) {
+			  return urls.push(response.json());
+			} else { 
+        return response.text()
+        console.log(response)
+			  throw new Error('Something is not working well');
+			}
+		})
+		.then(response => {
+			console.debug(response);
+			return new Response(response)
+		}).catch(error => {
+      
+			console.error(error);
+		});
 }
+
